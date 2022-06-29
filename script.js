e265280b630b8f1161f8aa70959df3d8a511d9bc
Sam Farrell <merriman2948@gmail.com>
2022-06-29 11:37:36 -0400

    Added query selectors to target all the buttons.

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
-const add = (a, b) => {
-	return a + b;
-};
+const zero = document.querySelector('.zero');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const addition = document.querySelector('.addition');
+const subtraction = document.querySelector('.subtraction');
+const multiplication = document.querySelector('.multiplication');
+const division = document.querySelector('.division');
+const power = document.querySelector('.power');
+const remainder = document.querySelector('.remainder');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
 
-const subtract = (a, b) => a - b;
+let displayValue = 0;
 
+const add = (a, b) => a + b;
 
-const multiply = (a, b) => a * b;
+const sub = (a, b) => a - b;
 
+const mult = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+const div = (a, b) => a / b;
 
-const power = (a, b) => a ** b;
+const pow = (a, b) => a ** b;
 
-const remainder = (a, b) => a % b;
+const rem = (a, b) => a % b;
 
 const operate = (operator, a, b) => {
     if(operator === "+") {
         return add(a, b);
     }
     if(operator === "-") {
-        return subtract(a, b);
+        return sub(a, b);
     }
     if(operator === "*") {
-        return multiply(a, b);
+        return mult(a, b);
     }
     if(operator === "/") {
-        return divide(a, b);
+        return div(a, b);
     }
     if(operator === "**") {
-        return power(a, b);
+        return pow(a, b);
     }
     if(operator === "%") {
-        return remainder(a, b);
+        return rem(a, b);
     }
 };
