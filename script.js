3e0b0cf589c1af4158af87ee8447205c4e13166b
Sam Farrell <merriman2948@gmail.com>
2022-06-29 10:13:59 -0400

    Changed divs to buttons in html. Added some button style via CSS. Created a basic calculator grid.

--- script.js
+++ script.js
@@ -2,25 +2,17 @@ const add = (a, b) => {
 	return a + b;
 };
 
-const subtract = (a, b) => {
-	return a - b;
-};
+const subtract = (a, b) => a - b;
 
-const multiply = (a, b) => {
-    return a * b;
-};
 
-const divide = (a, b) => {
-    return a / b;
-};
+const multiply = (a, b) => a * b;
 
-const power = (a, b) => {
-    return a ** b;
-};
 
-const remainder = (a, b) => {
-    return a % b;
-};
+const divide = (a, b) => a / b;
+
+const power = (a, b) => a ** b;
+
+const remainder = (a, b) => a % b;
 
 const operate = (operator, a, b) => {
     if(operator === "+") {
