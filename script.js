7bb4eb7bb0680cebb112cc5df0ba9e1f7a13c590
Sam Farrell <merriman2948@gmail.com>
2022-06-29 13:10:00 -0400

    Added a variable to hold display value. Created a variable area to populate. Made a update display function. Added event listeners to numbers one through nine. BIG update!

--- script.js
+++ script.js
@@ -1,23 +1,79 @@
-const zero = document.querySelector('.zero');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
-const addition = document.querySelector('.addition');
-const subtraction = document.querySelector('.subtraction');
-const multiplication = document.querySelector('.multiplication');
-const division = document.querySelector('.division');
-const power = document.querySelector('.power');
-const remainder = document.querySelector('.remainder');
-const clear = document.querySelector('.clear');
-const equals = document.querySelector('.equals');
-
+//Create variable to hold display value, create variable of display area and populate.
 let displayValue = 0;
+let displayArea = document.querySelector('.display');
+displayArea.textContent += displayValue;
+
+//Update display with button clicked
+const updateDisplay = (a) => {
+    displayValue = a;
+    displayArea.innerText = displayValue;
+};
+
+const zero = document.querySelector('.zero');
+zero.addEventListener('click', () => {
+updateDisplay(0);
+});
+
+const one = document.querySelector('.one');
+one.addEventListener('click', () => {
+updateDisplay(1);
+});
+
+const two = document.querySelector('.two');
+two.addEventListener('click', () => {
+updateDisplay(2);
+});
+
+const three = document.querySelector('.three');
+three.addEventListener('click', () => {
+updateDisplay(3);
+});
+
+const four = document.querySelector('.four');
+four.addEventListener('click', () => {
+updateDisplay(4);
+});
+
+const five = document.querySelector('.five');
+five.addEventListener('click', () => {
+updateDisplay(5);
+});
+
+const six = document.querySelector('.six');
+six.addEventListener('click', () => {
+updateDisplay(6);
+});
+
+const seven = document.querySelector('.seven');
+seven.addEventListener('click', () => {
+updateDisplay(7);
+});
+
+const eight = document.querySelector('.eight');
+eight.addEventListener('click', () => {
+updateDisplay(8);
+});
+
+const nine = document.querySelector('.nine');
+nine.addEventListener('click', () => {
+updateDisplay(9);
+});
+
+const addition = document.querySelector('.addition');
+
+const subtraction = document.querySelector('.subtraction');
+
+const multiplication = document.querySelector('.multiplication');
+
+const division = document.querySelector('.division');
+
+const power = document.querySelector('.power');
+
+const remainder = document.querySelector('.remainder');
+
+const clear = document.querySelector('.clear');
+
+const equals = document.querySelector('.equals');
 
 const add = (a, b) => a + b;
 
